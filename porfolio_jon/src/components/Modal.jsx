b816823576bb4e0c341a1c8d99251076e0d1674a
Irueta <irueta@gmail.com>
2024-01-23 13:59:12 +0100

    cv añadido

--- porfolio_jon/src/components/Modal.jsx
+++ porfolio_jon/src/components/Modal.jsx
@@ -15,23 +15,37 @@ function Modal({ type, setModal }) {
             case 'CV':
                 return (
                     <div className='modalContainer'>
-                        <h1 className='modalTitle'>CV</h1>
-                        <p className='modalText'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, maxime.
-                        </p>
                         <button className='modalButton' onClick={handleClick}>Cerrar</button>
+                      <iframe 
+                        className='modalPDF'
+                        src="https://drive.google.com/file/d/1ZB6PgAlAvuBkLeCkr2lt1RpHLLfwbe_c/preview" 
+                        width="640" 
+                        height="780"
+                      ></iframe>
                     </div>
-                );
-            case 'Contacto':
-                return (
-                    <div className='modalContainer'>
-                        <h1 className='modalTitle'>Contacto</h1>
-                        <p className='modalText'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, maxime.
-                        </p>
+                  );
+                  case 'Contacto':
+                      return (
+                          <div className='modalContainer'>
                         <button className='modalButton' onClick={handleClick}>Cerrar</button>
+                      <h1 className='modalTitle'>Contacto</h1>
+                      <form className='contactForm'>
+                        <label>
+                          Nombre:
+                          <input type='text' name='name' />
+                        </label>
+                        <label>
+                          Correo electrónico:
+                          <input type='email' name='email' />
+                        </label>
+                        <label>
+                          Mensaje:
+                          <textarea name='message' />
+                        </label>
+                        <input type='submit' value='Enviar' />
+                      </form>
                     </div>
-                );
+                  );
             case 'Proyectos':
                 return (
                     <div className='modalContainer'>
